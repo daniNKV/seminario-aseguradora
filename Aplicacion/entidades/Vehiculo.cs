4c0188ec90f5f6270d01711940f3aa319fda0780
daniNKV <nikiforovdaniel@gmail.com>
2023-05-08 19:36:51 -0300

    Agrega constructores

--- Aplicacion/entidades/Vehiculo.cs
+++ Aplicacion/entidades/Vehiculo.cs
@@ -5,7 +5,25 @@ public class Vehiculo: IAsegurable
     public static int cantidadAsegurados { get; set; } = 0;
     public int id { get; set; }
     public int titularId { get; set; } 
-    public String dominio { get; set; } = "Indefinido";
-    public String marca { get; set; } = "Indefinido";
-    public DateTime fabricacion { get; set; }
+    public string dominio { get; set; } = "Indefinido";
+    public string marca { get; set; } = "Indefinido";
+    public string fabricacion { get; set; } = "Indefinido";
+
+    public Vehiculo() {
+
+    }
+
+
+
+    public Vehiculo(string dominio, string marca, string fabricacion) {
+        this.dominio = dominio;
+        this.marca = marca;
+        this.fabricacion = fabricacion;
+    }
+
+
+    public override string ToString()
+    {
+        return $"{id},{titularId},{dominio},{marca},{fabricacion}";
+    }
 }
