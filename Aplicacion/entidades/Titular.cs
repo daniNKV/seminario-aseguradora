f77b7735127f5cea3bae1247dd3b5c907b9763b1
daniNKV <nikiforovdaniel@gmail.com>
2023-05-08 19:36:23 -0300

    Agrega listado de vehiculos para un titular

--- Aplicacion/entidades/Titular.cs
+++ Aplicacion/entidades/Titular.cs
@@ -2,19 +2,33 @@ namespace Aplicacion;
 
 public class Titular: Persona {
     public static int cantidadTitulares { get; set; } = 0;
-    public int _id { get; set; }
-    public string _direccion { get; set; } = "Indefinido";
-    public List<IAsegurable>? _asegurados { get; set; }
+    public int id { get; set; }
+    public string direccion { get; set; } = "Indefinido";
+    public string email { get; set; } = "Indefinido";
+    public List<Vehiculo>? ItemsAsegurados { get; set; }
 
-    public Titular(int dni, string nombre, string apellido): base(dni, nombre, apellido) {
-        _id = -1;
+    public Titular(): base(){
+        ItemsAsegurados = new List<Vehiculo>();
+    }
+    public Titular(int dni, string apellido, string nombre): base(dni, nombre, apellido) {
+        id = -1;
+        ItemsAsegurados = new List<Vehiculo>();
+
     }
     public Titular(int dni, string nombre, string apellido, string direccion): base(dni, nombre, apellido) {
-        _direccion = direccion;
+        id = -1;
+        ItemsAsegurados = new List<Vehiculo>();
+        this.direccion = direccion;
     }
 
     public override string ToString()
     {
-        return base.ToString() + $" {_direccion} + {_id}";
+        return base.ToString() + $" {id} {direccion} {email} ";
+    }
+    
+    public void agregarVehiculo(Vehiculo vehiculo) {
+        if (ItemsAsegurados != null) {
+            this.ItemsAsegurados.Add(vehiculo);
+        }
     }
 }
