c7680f6ec4e98fd21ce7da29648d0ef7ba81dac7
daniNKV <nikiforovdaniel@gmail.com>
2023-06-03 17:48:15 -0300

    Agrega Interfaz de persistencia de siniestros

---
+++ Aplicacion/interfaces/IRepositorioSiniestro.cs
@@ -0,0 +1,10 @@
+namespace Aplicacion;
+
+public interface IRepositorioSiniestro
+{
+    void AgregarSiniestro(Siniestro siniestro);
+    void EliminarSiniestro(int id);
+    void ModificarSiniestro(Siniestro siniestro);
+    List<Siniestro> ListarSiniestros();
+    List<Tercero> ListarTerceros(Siniestro siniestro);
+}
