580e2fc124f16d2572d37b77e19f844ba0c87eb1
daniNKV <nikiforovdaniel@gmail.com>
2023-06-16 22:58:13 -0300

    Agrega use cases como servicios al contenedor de dependencias

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,5 +1,9 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+
+using Aplicacion.UseCases.Asegurables;
+using Aplicacion.UseCases.Polizas;
+using Aplicacion.UseCases.Titulares;
+using Aplicacion.UseCases.Terceros;
+using Aplicacion.UseCases.Siniestros;
 using UI.Data;
 using MudBlazor.Services;
 
@@ -10,7 +14,42 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+// Add blazor components library
 builder.Services.AddMudServices();
+
+// Add use cases
+// Polizas
+builder.Services.AddTransient<AgregarPolizaUseCase>();
+builder.Services.AddTransient<ListarPolizasUseCase>();
+builder.Services.AddTransient<EliminarPolizaUseCase>();
+builder.Services.AddTransient<ModificarPolizaUseCase>();
+
+// Asegurables
+builder.Services.AddTransient<AgregarAsegurableUseCase>();
+builder.Services.AddTransient<ListarAsegurableUseCase>();
+builder.Services.AddTransient<EliminarAsegurableUseCase>();
+builder.Services.AddTransient<ModificarAsegurableUseCase>();
+
+// Titulares
+builder.Services.AddTransient<AgregarAsegurableUseCase>();
+builder.Services.AddTransient<ListarAsegurableUseCase>();
+builder.Services.AddTransient<ListarTitularesConSusAsegurablesUseCase>();
+builder.Services.AddTransient<EliminarAsegurableUseCase>();
+builder.Services.AddTransient<ModificarAsegurableUseCase>();
+
+// Siniestros
+builder.Services.AddTransient<AgregarSiniestroUseCase>();
+builder.Services.AddTransient<ListarSiniestroUseCase>();
+builder.Services.AddTransient<EliminarSiniestroUseCase>();
+builder.Services.AddTransient<ModificarSiniestroUseCase>();
+
+// Terceros
+builder.Services.AddTransient<AgregarTerceroUseCase>();
+builder.Services.AddTransient<ListarTerceroUseCase>();
+builder.Services.AddTransient<EliminarTerceroUseCase>();
+builder.Services.AddTransient<ModificarTerceroUseCase>();
+
 
 var app = builder.Build();
 
