6b6f4945e6a3389cce54ec87d93ab26327a3a1cf
daniNKV <nikiforovdaniel@gmail.com>
2023-06-19 01:34:07 -0300

    Corrige

--- Aplicacion/Entidades/Tercero.cs
+++ Aplicacion/Entidades/Tercero.cs
@@ -5,7 +5,7 @@ public class Tercero: Persona
     public int Id { get; set; }
     public string Aseguradora { get; set; } = "Indefinido";
     public int SiniestroId { get; set; } = -1;
-    public Siniestro? Siniestro { get => Siniestro; set => Siniestro = value;}
+    public Siniestro? Siniestro { get; set;}
 
     public Tercero(int dni, string nombre, string apellido): base(dni, nombre, apellido) {
 
@@ -20,4 +20,18 @@ public class Tercero: Persona
     Aseguradora = aseguradora;
     Siniestro = siniestro;
 }
+    
+
+    public Tercero(string nombre, string apellido, int dni, string aseguradora, string telefono): base(dni, nombre, apellido)
+    {
+        Nombre = nombre;
+        Apellido = apellido;
+        Dni = dni;
+        Aseguradora = aseguradora;
+        Telefono = telefono;
+    }
+
+    public Tercero()
+    {
+    }
 }
